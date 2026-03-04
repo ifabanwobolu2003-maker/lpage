9647bff559dc1fdc9a1cbd6ddff621db5ac0af00
Ifabanwo Boluwatife <ifabanwobolu2003@gmail.com>
2026-03-04 15:10:25 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
            {" "} THUB helps developers and creators build real-world tech skills through hands-on projects, expert mentorship, and a vibrant community. Join us and turn your ideas into reality.
         </span>
         </p>
-        <div id="pricing" className="flex justify-center my-10">
+        <div id="get-started" className="flex justify-center my-10">
             <a href="#pricing" className="bg-gradient-to-r from-blue-500 to-purple-500 text-white py-3 px-6 rounded-md text-lg">
                 Get Started
             </a>

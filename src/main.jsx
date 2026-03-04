e3131b117574fa97fddd84e42fcf44ae2da849a1
Ifabanwo Boluwatife <ifabanwobolu2003@gmail.com>
2026-03-04 15:52:37 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import './index.css'
 import App from './App.jsx'
 import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
